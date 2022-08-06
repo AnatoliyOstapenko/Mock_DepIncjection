19bc301d6e71149b6c7844fccb9967e6fb71337d
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-08-06 19:09:23 +0300

    Initial Commit

---
+++ Mock_DepIncjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mock_DepIncjection
+//
+//  Created by AnatoliiOstapenko on 06.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
